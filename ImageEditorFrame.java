f567027e3190ca77c183803c67019381b1998a80
Alex <waldekjulez@gmail.com>
2014-10-30 11:20:11 +0100

    Menubar Open geadded

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -5,6 +5,19 @@ import java.awt.image.*;
 import java.io.*; 
 import javax.imageio.*; 
 public class ImageEditorFrame extends JFrame{ 
+// HINZUGEFÜGT von BOB
+	private void createMenuBar(){ 
+			JMenuBar menuBar = new JMenuBar(); setJMenuBar(menuBar); JMenu menuFile = new JMenu("File"); menuBar.add(menuFile); JMenuItem menuItemOpen = new JMenuItem("Open"); menuFile.add(menuItemOpen); menuItemOpen.addActionListener( new ActionListener() {
+			Prüfen Sie den bisherigen Zwischenstand: Darstellung eines leeren Fensters ohne Titel
+			public void actionPerformed(ActionEvent e){
+			onOpen();
+			}
+		}
+	);
+}
+private void onOpen(){
+JOptionPane.showMessageDialog(this, "Open Selected");
+}
 	public static void main (String []args) {
 public ImageEditorFrame(){ 
 setDefaultCloseOperation(EXIT_ON_CLOSE); 
