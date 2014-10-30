a4d1d0616c3341dc8df625d97c73617c4323df42
annelieseLozano <anne@gmail.com>
2014-10-30 11:47:26 +0100

    in Frame neue Panel

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -4,7 +4,12 @@ import java.awt.*;
 import java.awt.image.*; 
 import java.io.*; 
 import javax.imageio.*; 
+
+
 public class ImageEditorFrame extends JFrame{ 
+
+panel = new ImageEditorPanel();
+add(panel);
 // HINZUGEFÜGT von BOB
 	private void createMenuBar(){ 
 			JMenuBar menuBar = new JMenuBar(); setJMenuBar(menuBar); JMenu menuFile = new JMenu("File"); menuBar.add(menuFile); JMenuItem menuItemOpen = new JMenuItem("Open"); menuFile.add(menuItemOpen); menuItemOpen.addActionListener( new ActionListener() {
@@ -18,6 +23,16 @@ public class ImageEditorFrame extends JFrame{
 private void onOpen(){
 JOptionPane.showMessageDialog(this, "Open Selected");
 }
+
+private void setDummyImage(){
+ BufferedImage bufferedImage = 
+ new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB);
+ Graphics g = bufferedImage.getGraphics();
+ g.setColor(Color.YELLOW);
+ g.fillOval(10, 10, 380, 280);
+ panel.setImage(bufferedImage);
+}
+
 	public static void main (String []args) {
 public ImageEditorFrame(){ 
 JFrame frame = new Jframe("Titel");
